fb3a02f53c7fa1da91a111a889f12b3737a99989
Roy Luo <royluo05@gmail.com>
2026-09-11 14:30:20 -0400

    Add: python-sorted-set-basics - submission-0

---
+++ Python Coding Interviews/python-sorted-set-basics/submission-0.py
@@ -0,0 +1,30 @@
+from typing import List
+from sortedcontainers import SortedSet
+
+
+def get_first_three(sorted_set: SortedSet[int], nums1: List[int], nums2: List[int]) -> List[int]:
+    
+    for num in nums1: 
+        sorted_set.add(num)
+    
+    for num in nums2: 
+        sorted_set.discard(num) 
+
+    list1 = [] 
+
+    for i in range(3): 
+        list1.append(sorted_set.pop(0)) 
+    
+    return list1
+    
+
+
+
+
+# take in a sorted set of integers and a 2 lists of integers the elements from first list should be added to the sorted set and the elements from list 2 should be remvoed reutnr the first 32 elements of the sorted set
+
+# do not modify below this line
+print(get_first_three(SortedSet(), [1, 2, 3], [4]))
+print(get_first_three(SortedSet([1, 4, 7, 2, 8, 9]), [10], [1, 7, 2]))
+print(get_first_three(SortedSet([1, 2, 3, 7]), [], [4, 5, 6]))
+print(get_first_three(SortedSet([1, 2, 3, 4, 5, 6, 7, 8, 9]), [10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
